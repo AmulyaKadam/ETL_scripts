5669bc4a24219603f63e4d563c1a06a8235ba6ab
Amulya kadam <kadamamulya017@gmail.com>
2025-12-10 20:17:29 +0530

    Update crm_cust_info.py

--- etl_project/silver/crm_cust_info.py
+++ etl_project/silver/crm_cust_info.py
@@ -46,8 +46,8 @@ def run(spark):
         rows_processed = clean_df.count()
 
         logging.info('Writing data to Silver layer...')
-        clean_df.write.format('parquet').mode('overwrite')\
-            .option('path',"/Volumes/workspace/default/mydata/silver/source_crm/crm_cust_info.parquet")\
+        clean_df.write.format('delta').mode('overwrite')\
+            .option('path',"/Volumes/workspace/default/mydata/silver/source_crm/crm_cust_info")\
                 .save()
 
     except Exception as e:
