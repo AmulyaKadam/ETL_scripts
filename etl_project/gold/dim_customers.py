32da0785ddd230d4f1ca615a88630c3bad7311d6
Amulya kadam <kadamamulya017@gmail.com>
2025-12-10 13:21:56 +0530

    Add files via upload

---
+++ etl_project/gold/dim_customers.py
@@ -0,0 +1,78 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+from datetime import datetime
+import logging
+
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger("dim_customers")
+
+def run(spark):
+    task = 'gold.dim_customers'
+    layer = 'gold'
+    rows_processed = 0
+    start = datetime.now()
+    error_message = None
+    status = 'SUCCESS'
+
+    try:
+        logging.info(f"Starting Task {task_name}...")
+
+
+        logging.info(f"Reading Silver data...")
+        crm_cust = spark.read.format("parquet").load("/Volumes/workspace/default/mydata/silver/source_crm/crm_cust_info.parquet")
+        erp_cust = spark.read.format("parquet").load("/Volumes/workspace/default/mydata/silver/source_erp/erp_cust_az12.parquet")
+        erp_loc  = spark.read.format("parquet").load("/Volumes/workspace/default/mydata/silver/source_erp/erp_loc_a101.parquet")
+
+
+        logging.info('Applying Trasnformations...')
+        w = Window.orderBy('cst_id')
+
+        df_final = (
+            crm_cust.alias('ci')
+            .join(erp_cust.alias('cb'), col('ci.cst_key') == col('cb.CID'),'left')
+            .join(erp_loc.alias('cl'), col('ci.cst_key') == col('cl.CID'),'left')
+            .select(
+                row_number().over(w).alias('Customer_key'),
+                col('ci.cst_id').alias('Customer_id'),
+                col('ci.cst_key').alias('Customer_number'),
+                col('ci.cst_firstname').alias('first_name'),
+                col('ci.cst_lastname').alias('last_name'),
+                col('cl.CNTRY').alias('Country'),
+                col('ci.cst_marital_status').alias('marital_status'),
+                when(col('ci.cst_gndr') != 'N/a',col('ci.cst_gndr')).otherwise(coalesce(col('cb.gen'),lit("N/a"))).alias('Gender'),
+                col('cb.BDATE').alias('Birthdate'),
+                col('ci.cst_create_date').alias('create_date')
+
+            )
+
+        )
+        rows_processed = df_final.count()
+
+        logging.info('Writing data to Gold layer...')
+        df_final.write.format('parquet').mode('overwrite')\
+            .option('path',"/Volumes/workspace/default/mydata/gold/dim_customers.parquet")\
+                .save()
+
+    except Exception as e:
+        logger.exception(f'Task {task} failed!')
+        status = 'FAILED'
+        rows_processed = 0
+        error_message = str(e)    
+    end = datetime.now()
+    logging.info(f"Task {task} completed in {end-start} seconds")
+    
+    return : 
+        {
+            'task_name': task,
+            'layer': layer,
+            'status': status,
+            'rows_processed': rows_processed,
+            'start_time': start,
+            'end_time': end,
+            'duration_seconds': (end-start).total_seconds(),
+            'error_message': error_message
+        }
---
+++ etl_project/gold/fact_sales.py
@@ -0,0 +1,75 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+from datetime import datetime
+import logging
+
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger("fact_sales")
+
+def run(spark):
+    task = 'gold.fact_sales'
+    layer = 'gold'
+    rows_processed = 0
+    start = datetime.now()
+    error_message = None
+    status = 'SUCCESS'
+
+    try:
+        logging.info(f"Starting Task {task_name}...")
+
+
+        logging.info(f"Reading Silver data...")
+        crm_sales = spark.read.format("parquet").load("/Volumes/workspace/default/mydata/silver/source_crm/crm_sales_details.parquet")
+        
+        logging.info(f"Reading gold dimentional data...")
+        dim_prd = spark.read.format("parquet").load("/Volumes/workspace/default/mydata/gold/dim_products.parquet")
+        dim_cst = spark.read.format("parquet").load("/Volumes/workspace/default/mydata/gold/dim_customers.parquet")
+
+
+        logging.info('Applying Trasnformations...')
+        df_final = (
+            crm_sales.alias('sd')
+            .join(dim_prd.alias('pd'), trim(col('sd.sls_prd_key')) == trim(col('pd.product_number')), 'left')
+            .join(dim_cst.alias('cd'), col('sd.sls_cust_id') == col('cd.Customer_id'), 'left')
+            .select(
+                col('sd.sls_ord_num').alias('order_number'),
+                col('pd.Product_key').alias('product_key'),
+                col('cd.Customer_key').alias('customer_key'),
+                col('sd.sls_order_dt').alias('order_date'),
+                col('sd.sls_ship_dt').alias('ship_date'),
+                col('sd.sls_due_dt').alias('due_date'),
+                col('sd.sls_sales').alias('sales_amount'),
+                col('sd.sls_quantity').alias('quantity'),
+                col('sd.fixed_price').alias('fixed_price')
+            )
+        )
+        rows_processed = df_final.count()
+
+        logging.info('Writing data to Gold layer...')
+        df_final.write.format('parquet').mode('overwrite')\
+            .option('path',"/Volumes/workspace/default/mydata/gold/fact_sales.parquet")\
+                .save()
+
+    except Exception as e:
+        logger.exception(f'Task {task} failed!')
+        status = 'FAILED'
+        rows_processed = 0
+        error_message = str(e)    
+    end = datetime.now()
+    logging.info(f"Task {task} completed in {end-start} seconds")
+    
+    return : 
+        {
+            'task_name': task,
+            'layer': layer,
+            'status': status,
+            'rows_processed': rows_processed,
+            'start_time': start,
+            'end_time': end,
+            'duration_seconds': (end-start).total_seconds(),
+            'error_message': error_message
+        }
