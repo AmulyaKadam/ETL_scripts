714801990cb0fcca17db8d8e4fecc43f2d08eb25
Amulya kadam <kadamamulya017@gmail.com>
2025-12-15 13:46:37 +0530

    Update crm_prd_info.py

--- etl_project/silver/crm_prd_info.py
+++ etl_project/silver/crm_prd_info.py
@@ -26,7 +26,7 @@ def run(spark):
         df = spark.read.format('csv')\
             .option("header", "true")\
                 .option("inferSchema", "true")\
-                    .load("/Volumes/workspace/default/source_crm/source_crm/prd_info.csv")  
+                    .load("abfss://source@awstorageamulya.dfs.core.windows.net/source_crm/prd_info.csv")  
 
         logging.info('Cleaning data...')
         window_spec = Window.partitionBy('prd_key').orderBy(col('prd_start_dt'))
@@ -44,7 +44,7 @@ def run(spark):
 
         logging.info('Writing data to Silver layer...')
         clean_df.write.format('delta').mode('overwrite')\
-            .option('path',"/Volumes/workspace/default/mydata/silver/source_crm/crm_prd_info")\
+            .option('path',"abfss://silver@awstorageamulya.dfs.core.windows.net/source_crm/crm_prd_info")\
                 .save()
 
     except Exception as e:
