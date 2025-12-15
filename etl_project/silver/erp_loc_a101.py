f8f2745f1b357839fb9af8142d623e0bd7d30a13
Amulya kadam <kadamamulya017@gmail.com>
2025-12-15 14:08:36 +0530

    Update erp_loc_a101.py

--- etl_project/silver/erp_loc_a101.py
+++ etl_project/silver/erp_loc_a101.py
@@ -26,7 +26,7 @@ def run(spark):
         df = spark.read.format('csv')\
                 .option("header", "true")\
                     .option("inferSchema", "true")\
-                        .load("abfss://source@awstorageamulya.dfs.core.windows.net/source_erp/LOC_A101.csv")
+                        .load("abfss://bronze@awstorageamulya.dfs.core.windows.net/source_erp/LOC_A101.csv")
 
 
         logging.info('Cleaning data...')
